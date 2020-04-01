61740bbdbde5cf901e3bbbe98fdaea3c55a615ad
Kyle <kylemcconnell94@gmail.com>
2020-04-01 13:38:30 -0500

    Done

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChristmasWithTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
